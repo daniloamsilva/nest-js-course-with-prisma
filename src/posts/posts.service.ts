64a5faf32e57a4e79a63d7e990946ac4ead90144
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-09-30 23:02:52 -0300

    Aula: Posts Controller e Service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsRepository } from './repositories/posts.repository';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly respository: PostsRepository) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.respository.create(createPostDto);
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.respository.findAll();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} post`;
+    return this.respository.findOne(id);
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+    return this.respository.update(id, updatePostDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    return this.respository.remove(id);
   }
 }
