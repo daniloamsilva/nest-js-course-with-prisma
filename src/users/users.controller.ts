6128b67f0df2b3ba557d8d2aa152006956603113
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-10-02 18:59:33 -0300

    Aula: Ajustando as API Responses na documentação

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiResponse({ status: 409, description: 'Conflito de e-mail' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
